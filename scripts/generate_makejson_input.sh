a27251821f138031dca16972223211d4bc2ecad5
samuelejones <samuel.e.jones@exeter.ac.uk>
2022-04-20 13:26:24 +0000

    config_meta_F2.tsv now written to correct folder

--- scripts/generate_makejson_input.sh
+++ scripts/generate_makejson_input.sh
@@ -32,7 +32,7 @@
    awk 'BEGIN{p["WE1.3"]=4;p["WQ1.3"]=3;p["NE1.3"]=2;p["NQ1.3"]=1;OFS="\t"}{split($1,a,"/");split(a[length(a)],b,"_")}{pheno=b[3];name=b[1];eth=b[7];name_eth=b[1]"_"b[7];ncases=b[8];ncontrols=b[9]}{ln="W1.3\t"name_eth"\t"$1"\t"ncases"\t"ncontrols"\t#CHR\tPOS\tREF\tALT\taf_alt\timputationInfo\tbeta\tbeta\tpval\tsebeta"}!(pheno in p){p[pheno]=5}!(name_eth in pp){pp[name_eth]=5}p[pheno]<pp[name_eth]{line[name_eth]=ln}END{for(ne in line){print line[ne]}}' munged_sumstats.txt
    # W2.3 prioritise "E" over "Q" and "W" over "N"
    awk 'BEGIN{p["WE2.3"]=4;p["WQ2.3"]=3;p["NE2.3"]=2;p["NQ2.3"]=1;OFS="\t"}{split($1,a,"/");split(a[length(a)],b,"_")}{pheno=b[3];name=b[1];eth=b[7];name_eth=b[1]"_"b[7];ncases=b[8];ncontrols=b[9]}{ln="W2.3\t"name_eth"\t"$1"\t"ncases"\t"ncontrols"\t#CHR\tPOS\tREF\tALT\taf_alt\timputationInfo\tbeta\tbeta\tpval\tsebeta"}!(pheno in p){p[pheno]=5}!(name_eth in pp){pp[name_eth]=5}p[pheno]<pp[name_eth]{line[name_eth]=ln}END{for(ne in line){print line[ne]}}' munged_sumstats.txt
-} > config_meta_F2.tsv
+} > data/DF2/config_meta_F2.tsv
 
 # clean up
 rm munged_sumstats.txt
